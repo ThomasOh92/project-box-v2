302102fcc76eca8836cd17c31f2bd672b64eca43
Thomas Oh <ohcsthomas@gmail.com>
2023-06-11 18:29:19 +0100

    Initial Scaffolding

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scaffolding Ready
+## Three different elements to incorporate
+## 1. React-Grid-Layout --> resizable, draggable, addable
+## 2. Material UI Speed Dial
+## 3. Add three types of items (googledocs/slides/sheets, websites, sticky notes) // we can have a think about the best 3 things that are links, we should try and have an opinion
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.tsx',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+            },
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: __dirname + '/dist',
+    },
+    devServer: {
+      static: './dist',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+    ],
+  };
+};
